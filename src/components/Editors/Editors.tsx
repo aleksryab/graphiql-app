9aa6cb6fd498bc082d8be6a6b71897ad83c0ffdd
Herman Kudria <h.kudrya@hotmail.com>
2023-05-09 21:08:18 +0200

    feat: try to add autocomplete for editor

--- src/components/Editors/Editors.tsx
+++ src/components/Editors/Editors.tsx
@@ -1,20 +1,60 @@
-import Editor from '@monaco-editor/react';
+import { buildClientSchema, getIntrospectionQuery, GraphQLSchema } from 'graphql';
+import MonacoEditor, { useMonaco } from '@monaco-editor/react';
+import { useEffect, useState } from 'react';
 
 interface EditorsProps {
-    isReadOnly: boolean,
-    language: EditorLanguage,
-    value?: string
+  isReadOnly: boolean;
+  language: EditorLanguage;
+  value?: string;
 }
 
 export enum EditorLanguage {
-    GRAPH_QL = 'graphql',
-    JSON = 'json'
+  GRAPH_QL = 'graphql',
+  JSON = 'json',
 }
 
-function Editors({isReadOnly, language, value}: EditorsProps) {
-    return (
-        <Editor language={language} value={value} theme="vs-dark" options={{readOnly: isReadOnly, minimap: {enabled: false}}}/>
-    );
+function Editors({ isReadOnly, language, value }: EditorsProps) {
+  const [schema, setSchema] = useState<GraphQLSchema>();
+
+  useEffect(() => {
+    if (language === EditorLanguage.GRAPH_QL) {
+      fetch('https://rickandmortyapi.com/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: getIntrospectionQuery() }),
+      })
+        .then((response) => response.json())
+        .then((json) => setSchema(buildClientSchema(json.data)));
+    }
+  }, []);
+
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    if (schema && monaco) {
+      monaco.languages.register({ id: 'graphql' });
+      monaco.languages.typescript.javascriptDefaults.addExtraLib(
+        JSON.stringify(schema),
+        'graphql-schema.json'
+      );
+    }
+  }, [schema, monaco]);
+
+  return (
+    <MonacoEditor
+      language={language}
+      value={value}
+      theme="vs-dark"
+      options={{
+        readOnly: isReadOnly,
+        minimap: { enabled: false },
+        suggestOnTriggerCharacters: true,
+        wordBasedSuggestions: false,
+      }}
+    />
+  );
 }
 
 export default Editors;
--- src/pages/EditorPage/EditorPage.tsx
+++ src/pages/EditorPage/EditorPage.tsx
@@ -1,46 +1,46 @@
-import './EditorPage.scss'
+import './EditorPage.scss';
 import Editors from '../../components/Editors';
-import {EditorLanguage} from '../../components/Editors/Editors';
+import { EditorLanguage } from '../../components/Editors/Editors';
 
 const EditorPage = () => {
-    const simpleJson = '{\n' +
-        '    "glossary": {\n' +
-        '        "title": "example glossary",\n' +
-        '\t\t"GlossDiv": {\n' +
-        '            "title": "S",\n' +
-        '\t\t\t"GlossList": {\n' +
-        '                "GlossEntry": {\n' +
-        '                    "ID": "SGML",\n' +
-        '\t\t\t\t\t"SortAs": "SGML",\n' +
-        '\t\t\t\t\t"GlossTerm": "Standard Generalized Markup Language",\n' +
-        '\t\t\t\t\t"Acronym": "SGML",\n' +
-        '\t\t\t\t\t"Abbrev": "ISO 8879:1986",\n' +
-        '\t\t\t\t\t"GlossDef": {\n' +
-        '                        "para": "A meta-markup language, used to create markup languages such as DocBook.",\n' +
-        '\t\t\t\t\t\t"GlossSeeAlso": ["GML", "XML"]\n' +
-        '                    },\n' +
-        '\t\t\t\t\t"GlossSee": "markup"\n' +
-        '                }\n' +
-        '            }\n' +
-        '        }\n' +
-        '    }\n' +
-        '}'
-    return (
-        <div className='containerEditor'>
-            <div className='inputEditor'>
-                <p>Editor</p>
-                <Editors isReadOnly={false} language={EditorLanguage.GRAPH_QL}/>
-            </div>
-            <div className='outputEditor'>
-                <p>Response</p>
-                <Editors isReadOnly={true} language={EditorLanguage.JSON} value={simpleJson}/>
-            </div>
-            <div className='variableEditor'>
-                <p>Variable</p>
-                <Editors isReadOnly={false} language={EditorLanguage.GRAPH_QL}/>
-            </div>
-        </div>
-    );
+  const simpleJson = `{
+    "glossary": {
+        "title": "example glossary",
+\t\t"GlossDiv": {
+            "title": "S",
+\t\t\t"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+\t\t\t\t\t"SortAs": "SGML",
+\t\t\t\t\t"GlossTerm": "Standard Generalized Markup Language",
+\t\t\t\t\t"Acronym": "SGML",
+\t\t\t\t\t"Abbrev": "ISO 8879:1986",
+\t\t\t\t\t"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+\t\t\t\t\t\t"GlossSeeAlso": ["GML", "XML"]
+                    },
+\t\t\t\t\t"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}`;
+  return (
+    <div className="containerEditor">
+      <div className="inputEditor">
+        <p>Editor</p>
+        <Editors isReadOnly={false} language={EditorLanguage.GRAPH_QL} />
+      </div>
+      <div className="outputEditor">
+        <p>Response</p>
+        <Editors isReadOnly={true} language={EditorLanguage.JSON} value={simpleJson} />
+      </div>
+      <div className="variableEditor">
+        <p>Variable</p>
+        <Editors isReadOnly={false} language={EditorLanguage.JSON} />
+      </div>
+    </div>
+  );
 };
 
 export default EditorPage;
