e6144c890e4785b86f754a19d6f4c95ac6dff10f
Anastasia Sytsevich <drozdushka@gmail.com>
2023-05-10 17:48:41 +0300

    feat: add styles to Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,27 @@
 import AuthorLink from './AuthorLink';
 import './Footer.scss';
+import { NavLink } from 'react-router-dom';
+import ROUTES from '../../constants/routes';
+import logo from '../../assets/footer_logo.svg';
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container container">
-        <div>
-          <AuthorLink name="aleksryab" link="https://github.com/aleksryab" />
-          <AuthorLink name="anastasiasyt" link="https://github.com/anastasiasyt" />
-          <AuthorLink name="hkudria" link="https://github.com/hkudria" />
+        <div className="">
+          <NavLink to={ROUTES.welcome}>
+            <img className="footer__logo" src={logo} alt="project logo" />
+          </NavLink>
         </div>
-        <div>2023</div>
+        <div className="footer__middle">
+          <div className="footer__links">
+            Students:
+            <AuthorLink name="aleksryab" link="https://github.com/aleksryab" />
+            <AuthorLink name="anastasiasyt" link="https://github.com/anastasiasyt" />
+            <AuthorLink name="hkudria" link="https://github.com/hkudria" />
+          </div>
+          <div className="footer__year">© all rights reserved, 2023.</div>
+        </div>
         <div>
           <a href="https://rs.school/react/" className="school-link">
             <span className="school-link__text">Rolling Scopes School</span>
